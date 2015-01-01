f25a4ac66d043fe1d75d5cfc74e126dbbcfb1644
at15 <at15@outlook.com>
2015-01-01 23:58:02 +0800

    Add symfony/console
    it is a  more powerful and testable commanline.
    much better than using echo and e...

---
+++ app/doubi.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: at15
+ * Date: 15-1-1
+ * Time: 下午11:52
+ */
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Symfony\Component\Console\Application;
+
+$console = new Application();
+$console->run();
---
+++ tests/config/database.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: at15
+ * Date: 15-1-1
+ * Time: 下午11:48
+ */
+return array(
+    'driver' => 'mysqli',
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => 'vagrant',
+    'database' => 'migration_test',
+    'db_debug' => TRUE,
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'stricton' => TRUE
+);
